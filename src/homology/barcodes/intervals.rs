c1e164b34ecb35285c217770d6252c83174bd54a
John Bochicchio <jpbochicchio@gmail.com>
2022-09-27 18:40:37 -0400

    Implement Display for Interval<T>. Add tests for Interval<T>

--- src/homology/barcodes/intervals.rs
+++ src/homology/barcodes/intervals.rs
@@ -2,7 +2,7 @@
 
 pub mod intervals {
     use core::cmp::Ordering::*;
-    use std::{fmt::Debug, ops::Sub};
+    use std::{fmt::{Debug, Display}, ops::Sub};
 
     const UID: i64 = -3434702188482864510;
 
@@ -118,6 +118,38 @@ pub mod intervals {
         T: PartialEq + PartialOrd + Debug + Clone,
     {
         fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            return write!(f, "{}", self.to_string());
+        }
+    }
+
+    impl<T> PartialOrd for Interval<T>
+    where
+        T: PartialEq + PartialOrd + Sub<Output = T> + Clone + Debug,
+    {
+        fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+            // Infinite cases
+            if self.clone().is_infinite() && other.clone().is_infinite() {
+                return Some(Equal);
+            }
+
+            if self.clone().is_infinite() {
+                return Some(Greater);
+            }
+
+            if other.clone().is_infinite() {
+                return Some(Less);
+            }
+
+            // Finite cases
+            let l_0: T = self.end.clone().unwrap() - self.start.clone().unwrap();
+            let l_1: T = other.end.clone().unwrap() - other.start.clone().unwrap();
+
+            return l_0.partial_cmp(&l_1);
+        }
+    }
+
+    impl<T> Display for Interval<T> where T: Clone + Debug {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
             let left_interval_symbol: &str;
             let left_value_str: String;
             let right_interval_symbol: &str;
@@ -150,34 +182,64 @@ pub mod intervals {
             return write!(
                 f,
                 "{}{}, {}{}",
-                left_interval_symbol, left_value_str, right_interval_symbol, right_value_str
+                left_interval_symbol, left_value_str, right_value_str, right_interval_symbol
             );
         }
     }
+}
 
-    impl<T> PartialOrd for Interval<T>
-    where
-        T: PartialEq + PartialOrd + Sub<Output = T> + Clone + Debug,
-    {
-        fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-            // Infinite cases
-            if self.clone().is_infinite() && other.clone().is_infinite() {
-                return Some(Equal);
-            }
+#[cfg(test)]
+mod tests {
+    use super::intervals::Interval;
+    use core::cmp::Ordering::*;
 
-            if self.clone().is_infinite() {
-                return Some(Greater);
-            }
+    #[test]
+    fn test_interval_printing() {
+        let finite_left_open_interval: Interval<i32> = Interval::finite_left_open_interval(2, 5);
+        let finite_right_open_interval: Interval<i32> = Interval::finite_right_open_interval(3, 6);
+        let finite_closed_interval: Interval<i32> = Interval::finite_closed_interval(-2, 3);
+        let finite_open_interval: Interval<i32> = Interval::finite_open_interval(-10, 10);
 
-            if other.clone().is_infinite() {
-                return Some(Less);
-            }
+        let left_infinite_open_interval: Interval<i32> = Interval::left_infinite_open_interval(10);
+        let right_infinite_open_interval: Interval<i32> = Interval::right_infinite_open_interval(10);
+        let left_infinite_closed_interval: Interval<i32> = Interval::left_infinite_closed_interval(10);
+        let right_infinite_closed_interval: Interval<i32> = Interval::right_infinite_closed_interval(10);
 
-            // Finite cases
-            let l_0: T = self.end.clone().unwrap() - self.start.clone().unwrap();
-            let l_1: T = other.end.clone().unwrap() - other.start.clone().unwrap();
+        assert_eq!(finite_left_open_interval.to_string(), String::from("(2, 5]"));
+        assert_eq!(finite_right_open_interval.to_string(), String::from("[3, 6)"));
+        assert_eq!(finite_closed_interval.to_string(), String::from("[-2, 3]"));
+        assert_eq!(finite_open_interval.to_string(), String::from("(-10, 10)"));
 
-            return l_0.partial_cmp(&l_1);
-        }
+        assert_eq!(left_infinite_open_interval.to_string(), String::from("(-infinity, 10)"));
+        assert_eq!(right_infinite_open_interval.to_string(), String::from("(10, infinity)"));
+        assert_eq!(left_infinite_closed_interval.to_string(), String::from("[-infinity, 10]"));
+        assert_eq!(right_infinite_closed_interval.to_string(), String::from("[10, infinity]"));
+    }
+
+    #[test]
+    fn test_finite_partial_order() {
+        let small_finite_interval: Interval<i16> = Interval::finite_open_interval(1, 3);
+        let large_finite_interval: Interval<i16> = Interval::finite_open_interval(-100, 100);
+
+        assert_eq!(small_finite_interval.partial_cmp(&large_finite_interval), Some(Less));
+        assert_eq!(large_finite_interval.partial_cmp(&small_finite_interval), Some(Greater));
+    }
+
+    #[test]
+    fn test_infinite_partial_order() {
+        let small_infinite_interval: Interval<i8> = Interval::left_infinite_open_interval(1);
+        let large_infinite_interval: Interval<i8> = Interval::right_infinite_closed_interval(100);
+
+        assert_eq!(small_infinite_interval.partial_cmp(&large_infinite_interval), Some(Equal));
+        assert_eq!(large_infinite_interval.partial_cmp(&small_infinite_interval), Some(Equal));
+    }
+
+    #[test]
+    fn test_mixed_partial_order() {
+        let finite_interval: Interval<i8> = Interval::finite_open_interval(1, 3);
+        let infinite_interval: Interval<i8> = Interval::left_infinite_open_interval(1);
+
+        assert_eq!(infinite_interval.partial_cmp(&finite_interval), Some(Greater));
+        assert_eq!(finite_interval.partial_cmp(&infinite_interval), Some(Less));
     }
 }
